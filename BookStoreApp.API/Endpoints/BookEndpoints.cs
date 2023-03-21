19aa43b0ca215b737fda3b104b7f6512f7d4966e
Lucas R Sena <msbowelite@gmail.com>
2023-03-21 10:33:43 -0300

    ApplyingEndpoints Concept

---
+++ BookStoreApp.API/Endpoints/BookEndpoints.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using BookStoreApp.API.Data;
+using BookStoreApp.API.Endpoints.Internal;
+using BookStoreApp.API.Models.Book;
+using BookStoreApp.API.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.IdentityModel.Tokens;
+
+namespace BookStoreApp.API.Endpoints
+{
+    public class BookEndpoints : IEndpoints
+    {
+        private const string ContentType = "application/json";
+        private const string Tag = "Books";
+        private const string BaseRoute = "books";
+
+        public static void AddServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<IBooksRepository, BooksRepository>();
+        }
+
+        public static void DefineEndpoints(IEndpointRouteBuilder app)
+        {
+            app.MapPost(BaseRoute, CreateBookAsync)
+                //.RequireAuthorization("Administrator")
+                .WithName("CreateBook")
+                .Accepts<BookCreateDto>(ContentType)
+                .Produces<Book>(201)
+                .Produces<IEnumerable<ValidationFailure>>(400)
+                .WithTags(Tag);
+        }
+
+        internal static async Task<IResult> CreateBookAsync(
+            BookCreateDto bookDto, IMapper mapper, IBooksRepository booksRepository)
+        {
+            var book = mapper.Map<Book>(bookDto);
+            await booksRepository.AddAsync(book);
+
+            return Results.Created($"/{BaseRoute}/{book.Isbn}", book);
+        }
+    }
+}
--- BookStoreApp.API/Program.cs
+++ BookStoreApp.API/Program.cs
@@ -1,5 +1,6 @@
 using BookStoreApp.API.Configurations;
 using BookStoreApp.API.Data;
+using BookStoreApp.API.Endpoints.Internal;
 using BookStoreApp.API.Repositories;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
@@ -11,6 +12,12 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpoints<Program>(builder.Configuration);
+
+//builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+
 // Add services to the container.
 var connString = builder.Configuration.GetConnectionString("BookStoreAppDbConnection");
 builder.Services.AddDbContext<BookStoreDbContext>(options => options.UseSqlServer(connString));
@@ -20,15 +27,6 @@ builder.Services.AddIdentityCore<ApiUser>()
     .AddEntityFrameworkStores<BookStoreDbContext>();
 
 builder.Services.AddAutoMapper(typeof(MapperConfig));
-
-builder.Services.AddScoped<IAuthorsRepository, AuthorsRepository>();
-builder.Services.AddScoped<IBooksRepository, BooksRepository>();
-
-builder.Services.AddControllers();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 builder.Host.UseSerilog((ctx, lc) => 
     lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
@@ -40,6 +38,7 @@ builder.Services.AddCors( options => {
         .AllowAnyOrigin());
 });
 
+builder.Services.AddAuthorization();
 builder.Services.AddAuthentication(options => {
     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
     options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -65,6 +64,9 @@ if (app.Environment.IsDevelopment())
 {
     
 }
+
+app.UseEndpoints<Program>();
+
 app.UseSwagger();
 app.UseSwaggerUI();
 
@@ -75,7 +77,5 @@ app.UseCors("AllowAll");
 
 app.UseAuthentication();
 app.UseAuthorization();
-
-app.MapControllers();
 
 app.Run();
